01577ef73df2b1fcaa7015fefd80cc505b33b802
akiriik <120096481+akiriik@users.noreply.github.com>
2024-11-23 18:33:48 +0200

    Update ns2009.h

--- components/ns2009/ns2009.h
+++ components/ns2009/ns2009.h
@@ -2,48 +2,37 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/touchscreen/touchscreen.h"
 
 namespace esphome {
 namespace ns2009 {
 
-#define DEFAULT_NS2009_ADDR 0x48
-#define THRESHOLD_Z 150
 #define NS2009_READ_X 0xC0
 #define NS2009_READ_Y 0xD0
 #define NS2009_READ_Z 0xE0
-#define CALIBRATE_MIN_X 200
-#define CALIBRATE_MIN_Y 400
-#define CALIBRATE_MAX_X 3850
-#define CALIBRATE_MAX_Y 3920
-#define SCREEN_SIZE_X 240
-#define SCREEN_SIZE_Y 320
+#define TOUCH_THRESHOLD 150
 
-class NS2009Component : public Component, public i2c::I2CDevice {
- public:
+class NS2009Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevice {
+public:
   void setup() override;
   void loop() override;
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  int raw_x{0}, x{0}, raw_y{0}, y{0}, raw_z{0};
-  bool touched{false};
-  bool flip_x{false};
-  bool flip_y{false};
-
-  void calibrate(int min_x, int max_x, int min_y, int max_y);
-  void set_flip_x(bool flip) { this->flip_x = flip; }
-  void set_flip_y(bool flip) { this->flip_y = flip; }
-
- protected:
-  int min_x_{CALIBRATE_MIN_X};
-  int max_x_{CALIBRATE_MAX_X}; 
-  int min_y_{CALIBRATE_MIN_Y};
-  int max_y_{CALIBRATE_MAX_Y};
-
-  unsigned int read_register_(unsigned char command);
-  int map_data_(int data, int in_min, int in_max, int out_min, int out_max);
-  bool check_touched_();
-  void scan_();
+protected:
+  uint16_t read_register_(uint8_t reg);
+  bool read_touch_point_();
+  
+  uint16_t raw_x_{0};
+  uint16_t raw_y_{0};
+  uint16_t raw_z_{0};
+  
+  static constexpr uint16_t min_x_ = 410;
+  static constexpr uint16_t max_x_ = 3780;
+  static constexpr uint16_t min_y_ = 360;
+  static constexpr uint16_t max_y_ = 3770;
+  static constexpr uint16_t screen_width_ = 320;
+  static constexpr uint16_t screen_height_ = 240;
 };
 
 }  // namespace ns2009
