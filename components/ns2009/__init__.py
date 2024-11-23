091b3bed6ee540e45f7e287aa38bc286e16b2524
akiriik <120096481+akiriik@users.noreply.github.com>
2024-11-23 18:44:41 +0200

    Update __init__.py

--- components/ns2009/__init__.py
+++ components/ns2009/__init__.py
@@ -1,19 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, touchscreen
+from esphome.components import i2c
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['i2c']
-AUTO_LOAD = ['touchscreen']
 
 ns2009_ns = cg.esphome_ns.namespace('ns2009')
-NS2009Touchscreen = ns2009_ns.class_('NS2009Touchscreen', touchscreen.Touchscreen, i2c.I2CDevice)
+NS2009Component = ns2009_ns.class_('NS2009Touchscreen', cg.Component, i2c.I2CDevice)
 
-CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(NS2009Touchscreen),
-}).extend(i2c.i2c_device_schema(0x48))
+CONF_NS2009_ID = 'ns2009_id'
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(NS2009Component),
+}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x48))
 
 async def to_code(config):
-    var = await touchscreen.new_touchscreen(config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
