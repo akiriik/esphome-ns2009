0b4b7ea53308ba507a8024a879f98ca4fc18130e
akiriik <120096481+akiriik@users.noreply.github.com>
2025-01-10 18:08:05 +0200

    Update ns2009.h

--- components/ns2009/ns2009.h
+++ components/ns2009/ns2009.h
@@ -1,34 +1,34 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/hal.h"
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace ns2009 {
 
-#define NS2009_READ_X 0xC0
-#define NS2009_READ_Y 0xD0
-#define NS2009_READ_Z 0xE0
-#define TOUCH_THRESHOLD 150
+// Rekisterit
+static const uint8_t REG_X = 0xC0;  
+static const uint8_t REG_Y = 0xD0;
+// Poistetaan Z-rekisterit koska emme tarvitse niitä
 
-class NS2009Touchscreen : public Component, public i2c::I2CDevice {
+class NS2009Component : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
-  void loop() override;
   void dump_config() override;
+  void loop() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // Julkiset muuttujat kosketuksen lukemiseen
-  bool touched = false;
-  int x = 0;
-  int y = 0;
+  // Uusi optio nopeille luvuille
+  void set_fast_reads(bool fast_reads) { fast_reads_ = fast_reads; }
+
+  float x{0};
+  float y{0};
+  // Poistetaan z koska emme tarvitse sitä
 
  protected:
-  uint16_t read_register_(uint8_t reg);
-  
-  uint16_t raw_x_{0};
-  uint16_t raw_y_{0};
-  uint16_t raw_z_{0};
+  uint8_t read_byte_(uint8_t a_register);
+  bool fast_reads_{false};  // Uusi muuttuja nopeille luvuille
 };
 
 }  // namespace ns2009
