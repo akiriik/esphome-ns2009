6ca25dcad8e5f8bd7d37faf75d57315ae0747e82
akiriik <120096481+akiriik@users.noreply.github.com>
2025-01-10 18:11:35 +0200

    Update __init__.py

--- components/ns2009/__init__.py
+++ components/ns2009/__init__.py
@@ -4,17 +4,26 @@ from esphome.components import i2c
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['i2c']
+AUTO_LOAD = ['sensor']
 
 ns2009_ns = cg.esphome_ns.namespace('ns2009')
-NS2009Component = ns2009_ns.class_('NS2009Touchscreen', cg.Component, i2c.I2CDevice)
+NS2009Component = ns2009_ns.class_('NS2009Component', cg.Component, i2c.I2CDevice)
 
-CONF_NS2009_ID = 'ns2009_id'
+CONF_FAST_READS = 'fast_reads'
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(NS2009Component),
-}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x48))
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(NS2009Component),
+        cv.Optional(CONF_FAST_READS, default=False): cv.boolean,
+    })
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x48))
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
+    
+    if CONF_FAST_READS in config:
+        cg.add(var.set_fast_reads(config[CONF_FAST_READS]))
