7a592a8f7f5933d27da8e3d5eb55227f093c7a99
akiriik <120096481+akiriik@users.noreply.github.com>
2025-01-10 18:08:26 +0200

    Update ns2009.cpp

--- components/ns2009/ns2009.cpp
+++ components/ns2009/ns2009.cpp
@@ -6,48 +6,43 @@ namespace ns2009 {
 
 static const char *const TAG = "ns2009";
 
-// Lisätään oma map funktio
-int map_value(int value, int in_min, int in_max, int out_min, int out_max) {
-  if (value < in_min) value = in_min;
-  if (value > in_max) value = in_max;
-  return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void NS2009Touchscreen::setup() {
+void NS2009Component::setup() {
   ESP_LOGCONFIG(TAG, "Setting up NS2009...");
 }
 
-void NS2009Touchscreen::loop() {
-  do {
-    this->raw_z_ = read_register_(NS2009_READ_Z);
-  } while (this->raw_z_ == 0xFFF);  // Filter false readings
-
-  this->touched = (this->raw_z_ > TOUCH_THRESHOLD);
-  
-  if (this->touched) {
-    this->raw_x_ = read_register_(NS2009_READ_X);
-    this->raw_y_ = read_register_(NS2009_READ_Y);
-    
-    // Käytetään omaa map_value funktiota
-    this->x = map_value(this->raw_x_, 410, 3780, 0, 320);
-    this->y = map_value(this->raw_y_, 360, 3770, 0, 240);
-    
-    ESP_LOGD(TAG, "Touch detected - X: %d, Y: %d, Z: %d", this->x, this->y, this->raw_z_);
-  }
-}
-
-uint16_t NS2009Touchscreen::read_register_(uint8_t reg) {
-  uint8_t data[2];
-  if (!this->read_bytes(reg, data, 2)) {
-    ESP_LOGW(TAG, "Reading register %02x failed!", reg);
-    return 0;
-  }
-  return (data[0] << 4) | (data[1] >> 4);
-}
-
-void NS2009Touchscreen::dump_config() {
+void NS2009Component::dump_config() {
   ESP_LOGCONFIG(TAG, "NS2009:");
   LOG_I2C_DEVICE(this);
+  if (this->fast_reads_) {
+    ESP_LOGCONFIG(TAG, "  Fast reads enabled");
+  }
+}
+
+uint8_t NS2009Component::read_byte_(uint8_t a_register) {
+  uint8_t ret;
+  this->write(&a_register, 1);
+  this->read(&ret, 1);
+  return ret;
+}
+
+void NS2009Component::loop() {
+  uint8_t x_low = this->read_byte_(REG_X);
+  uint8_t x_high = this->read_byte_(REG_X);
+  uint8_t y_low = this->read_byte_(REG_Y);
+  uint8_t y_high = this->read_byte_(REG_Y);
+
+  // Yhdistetään bitit
+  uint16_t x_raw = (x_high << 8) | x_low;
+  uint16_t y_raw = (y_high << 8) | y_low;
+
+  // Muunnetaan arvot floateiksi
+  this->x = x_raw;
+  this->y = y_raw;
+
+  // Jos fast_reads ei ole päällä, lisätään pieni viive
+  if (!this->fast_reads_) {
+    delayMicroseconds(100);  // Pienennetty 10ms -> 100µs
+  }
 }
 
 }  // namespace ns2009
